63a885560cb8384cfbfe988d100db5ab63792904
DavidGumm <gumm.david@gmail.com>
2020-04-07 23:13:07 -0700

    updated data files.

--- Data/db-xml.ps1
+++ Data/db-xml.ps1
@@ -1,27 +1,46 @@
-
+CLS
 Add-Type -AssemblyName System;
 Add-Type -AssemblyName System.Data;
 Add-Type -AssemblyName System.Data.Linq;
 Add-Type -AssemblyName System.Collections;
 
-#[xml]$db = Get-Content -Path db.xml
+#[xml]$db = Get-Content -Path "data\db.xml"
 
 $FrontEnd = $db.root.tables.category[0]
 $BackEnd = $db.root.tables.category[1]
 
-$array = [System.Collections.Generic.List[object]]::new();
+$mainTables = $FrontEnd, $BackEnd;
 
-foreach($front in $BackEnd.ChildNodes)
+
+$Items = [System.Collections.Generic.List[object]]::new();
+
+$tables = @{};
+
+foreach($category in $mainTables)
 {
-    $results = $front.tablerows.ChildNodes
-    foreach($result in $results)
+    foreach($child1 in $category.ChildNodes)
     {
-        $array.Add($result.results.FirstChild.result.'#text');
+        foreach($child2 in $child1.tablerows.ChildNodes)
+        {
+            foreach($child3 in $child2.results.ChildNodes)
+            {
+                $Items.Add($child3.result.'#text');
+            }
+        }
+        
+        $Title = $child1.name.'#text'
+        $key = $Title -replace "\d\d - ", "" -replace "\s+","" -replace "[!,(,),-,-,+,\d,\.]+",""
+        $Description = $child1.description.'#text'
+
+        $tables[$key] = @{
+            Title = $Title
+            Description = $Description
+            Items = $Items
+        }
+        $Items = [System.Collections.Generic.List[object]]::new();
     }
-    "$($Item.Title)"
-    $Item = @{};
-    $Item.Title = $front.name.'#text';
-    $Item.Description = $front.description.'#text';
-    $Item.Items = $array
-    $Item | ConvertTo-Json | Set-Content "$($Item.Title).JSON";
 }
+
+
+$tables | ConvertTo-Json | Set-Content C:\Users\gummd\source\repos\RPGenie\Data\db.JSON
+$tables | ConvertTo-Json -Compress | Set-Content C:\Users\gummd\source\repos\RPGenie\Data\db.compressed.JSON
--- js/main.js
+++ js/main.js
@@ -1,90 +1,90 @@
 const RPGenie = function (seed) {
   RPGenie.prototype.seed = new Math.seedrandom(seed);
+};
 
-  RPGenie.prototype.RandomBetween = function (max) {
-    return Math.floor(this.seed() * Math.floor(max));
-  };
+const RandomBetween = function (max) {
+  return Math.floor(this.seed() * Math.floor(max));
+};
 
-  RPGenie.prototype.Item = () => {};
+const Item = () => {};
 
-  RPGenie.prototype.Building = function () {
-    return { Item: null };
-  };
+const Building = function () {
+  return { Item: null };
+};
 
-  RPGenie.prototype.Type = () => {
-    let types = [
-      "Organizational",
-      "Shop",
-      "Shop",
-      "Shop",
-      "Home",
-      "Home",
-      "Home",
-      "Home",
-      "Home",
-      "Home",
-    ];
-    return types[this.RandomBetween(types.length)];
-  };
+const Type = () => {
+  let types = [
+    "Organizational",
+    "Shop",
+    "Shop",
+    "Shop",
+    "Home",
+    "Home",
+    "Home",
+    "Home",
+    "Home",
+    "Home",
+  ];
+  return types[this.RandomBetween(types.length)];
+};
 
-  RPGenie.prototype.Subtype = (building) => {
-    let subtype = [];
-    switch (building.Type) {
-      case "Home":
-        subtype = [
-          "Rich",
-          "Middle",
-          "Middle",
-          "Middle",
-          "Poor",
-          "Poor",
-          "Poor",
-          "Poor",
-          "Poor",
-          "Poor",
-          "Poor",
-          "Poor",
-          "Poor",
-          "Poor",
-          "Poor",
-          "Poor",
-          "Poor",
-          "Poor",
-          "Poor",
-          "Poor",
-        ];
-        break;
-      case "Organizational":
-        subtype = ["Government", "Guild", "Religious"];
-        break;
-      case "Shop":
-        subtype = [
-          "Tavern",
-          "Restaurant",
-          "Inn",
-          "Brothel",
-          "Blacksmith",
-          "General Goods",
-          "Magic",
-          "Tech Shop",
-        ];
-        break;
-      default:
-        break;
-    }
-    return subtype[this.RandomBetween(subtype.length)];
-  };
-  RPGenie.prototype.Owner = () => {};
-  RPGenie.prototype.Description = () => {};
-  RPGenie.prototype.Name = () => {};
+const Subtype = (building) => {
+  let subtype = [];
+  switch (building.Type) {
+    case "Home":
+      subtype = [
+        "Rich",
+        "Middle",
+        "Middle",
+        "Middle",
+        "Poor",
+        "Poor",
+        "Poor",
+        "Poor",
+        "Poor",
+        "Poor",
+        "Poor",
+        "Poor",
+        "Poor",
+        "Poor",
+        "Poor",
+        "Poor",
+        "Poor",
+        "Poor",
+        "Poor",
+        "Poor",
+      ];
+      break;
+    case "Organizational":
+      subtype = ["Government", "Guild", "Religious"];
+      break;
+    case "Shop":
+      subtype = [
+        "Tavern",
+        "Restaurant",
+        "Inn",
+        "Brothel",
+        "Blacksmith",
+        "General Goods",
+        "Magic",
+        "Tech Shop",
+      ];
+      break;
+    default:
+      break;
+  }
+  return subtype[this.RandomBetween(subtype.length)];
+};
+const Owner = () => {};
+const Description = () => {};
+const Name = () => {};
 
-  RPGenie.prototype.BuildingBuild = () => {
-    var building = new this.Building();
-    building.Type = this.Type();
-    building.Subtype = this.Subtype(building);
-    building.Owner = this.Owner(building);
-    building.Description = this.Description(building);
-    building.Name = this.Name(building);
-    return building;
-  };
+const BuildingBuild = () => {
+  var building = new Building();
+  building.Type = this.Type();
+  building.Subtype = this.Subtype(building);
+  building.Owner = this.Owner(building);
+  building.Description = this.Description(building);
+  building.Name = this.Name(building);
+  return building;
 };
