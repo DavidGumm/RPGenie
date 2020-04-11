e3c35322c03673a4b367d168caebc6ae2e249acb
DavidGumm <gumm.david@gmail.com>
2020-04-10 22:50:35 -0700

    end of day commit

--- dataparser/Program.cs
+++ dataparser/Program.cs
@@ -5,6 +5,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.IO;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Converters;
+using System.Diagnostics;
 #nullable enable
 
 namespace dataparser
@@ -13,42 +16,62 @@ namespace dataparser
     {
         static void Main(string[] args)
         {
-            var dataPath = $"{Directory.GetCurrentDirectory()}\\..\\Data\\JSON";
-            var files = DirSearch(dataPath);
+            foreach (string arg in args)
+            {
+                switch (arg.Substring(0, 2).ToLower())
+                {
+                    case "-clear":
+                        Console.Clear();
+                        break;
+                    default:
+                        // do other stuff...
+                        break;
+                }
+            }
 
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            
+            Console.WriteLine("Starting parser.");
+            var dataPath = $"{Directory.GetCurrentDirectory()}\\..\\Data\\JSON\\";
+            var files = DirSearch(dataPath);
+            Console.WriteLine($"Files located: {files.Count }");
+
+            JObject? table = null;
+            Console.WriteLine($"Reading files.");
+            int count = 1;
             foreach (string f in files)
             {
-                var table = Newtonsoft.Json.JsonConvert.DeserializeObject<Table>(System.IO.File.ReadAllText(f));
-                var stringData = Newtonsoft.Json.JsonConvert.SerializeObject(table);
-                System.IO.File.WriteAllText(f, stringData);
+                var path = f.Replace(dataPath, "").Replace(".JSON", "").Split("\\");
+                Console.Write($"\rReading file {count++} - {path[path.Count()-1]}");
+
+                //table = JObject.Parse(System.IO.File.ReadAllText(f));
+                //var tableName = table.Properties().Select(p => p.Name).FirstOrDefault();
+                //table[tableName]
+                //var stringData = Newtonsoft.Json.JsonConvert.SerializeObject(table[tableName]);
+
+
+                //System.IO.File.WriteAllText(f, stringData);
+                //return;
             }
+            stopwatch.Start();
+            Console.WriteLine();
+            Console.WriteLine("Finished reading files.");
+            Console.WriteLine($"Runtime: {stopwatch.ElapsedMilliseconds.ToString()}ms.");
         }
 
-        public class Table
-        { 
-            public Dictionary<String, data>? Data { get; set; }
-        }
+        static JObject setDatabase(JObject data, List<string> Name, JObject subData, int depth, int count) {
+        if (depth == 0) return data;
 
-        public class data 
-        {
-            //public data? Data { get; set; }
-            public String? Title { get; set; }
-            public String? Description { get; set; }
-            public List<String>? Items { get; set; }
-         }
-
-        // static Object setDatabase(Dictionary<string, object> data, List<string> Name, string subData, int depth, int count) {
-        // if (depth == 0) return data;
-
-        // if (Name.Count() > 1) {
-        //     Name = Name.Skip(1).ToList();
+        if (Name.Count() > 1) {
+            Name = Name.Skip(1).ToList();
             
-        //     data[Name[0]] = setDatabase(data[Name[0]], Name, subData, --depth, count);
-        // } else {
-        //     data[Name[0]] = subData[Name[0]];
-        // }
-        // return data;
-        // }
+            //data[Name[0]] = setDatabase(data[Name[0]], Name, subData, --depth, count);
+        } else {
+            data[Name[0]] = subData[Name[0]];
+        }
+        return data;
+        }
 
         static List<string> DirSearch(string sDir)
         {
