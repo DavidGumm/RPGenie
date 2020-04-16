c9603af1fb243fad0ec03221324e0355b265a918
DavidGumm <gumm.david@gmail.com>
2020-04-15 20:15:09 -0700

    added city generation to site.

--- js/main.js
+++ js/main.js
@@ -6,22 +6,23 @@ const RPGenie = function (seed) {
   };
 
   const citySize = [
-    { Name: "Megalopolis", size: { min: 10000000, max: 0 } },
-    { Name: "Conurbation", size: { min: 3000000, max: 0 } },
-    { Name: "Metropolis", size: { min: 1000000, max: 0 } },
-    { Name: "Large city", size: { min: 300000, max: 0 } },
-    { Name: "Medium city", size: { min: 150000, max: 0 } },
-    { Name: "Small City", size: { min: 100000, max: 0 } },
-    { Name: "Large town", size: { min: 10000, max: 0 } },
-    { Name: "Town", size: { min: 150, max: 0 } },
-    { Name: "Village", size: { min: 50, max: 0 } },
-    { Name: "Hamlet", size: { min: 5, max: 0 } },
-    { Name: "Homestead", size: { min: 1, max: 0 } },
+    { Name: "Homestead", size: { min: 1, max: 5 } },
+    { Name: "Hamlet", size: { min: 5, max: 50 } },
+    { Name: "Village", size: { min: 50, max: 150 } },
+    { Name: "Town", size: { min: 150, max: 10000 } },
+    { Name: "Large town", size: { min: 10000, max: 100000 } },
+    { Name: "Small City", size: { min: 100000, max: 150000 } },
+    { Name: "Medium city", size: { min: 150000, max: 300000 } },
+    { Name: "Large city", size: { min: 300000, max: 1000000 } },
+    { Name: "Metropolis", size: { min: 1000000, max: 3000000 } },
+    { Name: "Conurbation", size: { min: 3000000, max: 10000000 } },
+    { Name: "Megalopolis", size: { min: 10000000, max: 9999999999 } },
   ];
 
   RPGenie.prototype.City = function () {
-    this.Name = 0;
-    this.Size = 0;
+    this.Name = "CITYNAME";
+    this.Type = "CityType";
+    this.Population = 0;
     this.BuildingCount = 0;
     return this;
   };
@@ -84,32 +85,19 @@ const RPGenie = function (seed) {
     return building;
   };
   RPGenie.prototype.Quality = (item) => {
-    let quality = [
-      "Rich",
-      "Middle",
-      "Middle",
-      "Middle",
-      "Poor",
-      "Poor",
-      "Poor",
-      "Poor",
-      "Poor",
-      "Poor",
-      "Poor",
-      "Poor",
-      "Poor",
-      "Poor",
-      "Poor",
-      "Poor",
-      "Poor",
-      "Poor",
-      "Poor",
-      "Poor",
-    ];
-
-    item.Quality = quality[this.RandomBetween(quality.length)];
+    let qualityTypes = ["Rich", "Middle", "Poor"];
+    let quality = [];
+    qualityTypes.forEach((type, ind) => {
+      let rounds = Math.round(Math.pow(ind + 1, 3.9));
+      for (let index = 0; index < rounds; index++) {
+        quality.push(type);
+      }
+    });
+    let index = this.RandomBetween(quality.length);
+    item.Quality = quality[index];
     return item;
   };
+
   RPGenie.prototype.Name = (building) => {
     let names = [];
     switch (building.subtype) {
@@ -135,24 +123,34 @@ const RPGenie = function (seed) {
     return building;
   };
 
-  RPGenie.prototype.CityBuild = () => {
-    var city = new this.City();
-    //city.Name = this.City.Name(city);
-
-    let size = [];
+  RPGenie.prototype.citySize = (city) => {
+    let citySizes = citySize.filter((item, index) => index < 8).reverse();
 
     let sizes = [];
+    citySizes.forEach((item) => {
+      let rounds = Math.round(Math.pow(5, 2.1));
+      for (let index = 0; index < rounds; index++) {
+        sizes.push(item);
+      }
+    });
+    let size = sizes[this.RandomBetween(sizes.length)];
+    let max = size.size.max - size.size.min;
+    city.Population = this.RandomBetween(max) + size.size.min;
+    city.BuildingCount = Math.round(
+      (this.RandomBetween(max) + size.size.min) / 10
+    );
+    city.Type = size.Name;
+    return city;
+  };
 
-    for (let index = 1; index < citySize.length + 1; index++) {
-      sizes.push(Math.pow(index, 10));
-    }
+  RPGenie.prototype.CityName = (city) => {
+    return city;
+  };
 
-    for (let index = 0; index < 10000000; index++) {
-      const element = array[index];
-      size.push(citySize);
-    }
-
-    city.BuildingCount = size;
+  RPGenie.prototype.cityBuild = () => {
+    var city = new this.City();
+    city = this.citySize(city);
+    city = this.CityName(city);
     return city;
   };
 
