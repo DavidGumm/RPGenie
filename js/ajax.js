277b65df364cca4f3e3a054f429d18506e5cc32f
David <gumm.david@gmail.com>
2020-05-01 08:08:49 -0700

    Update ajax.js

--- js/ajax.js
+++ js/ajax.js
@@ -1,8 +1,11 @@
-const Ajax = function (type, uri, success, params) {
+const Ajax = function (type, uri, params, success, failure) {
   const xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       success(this.responseText, this);
+    }
+    else if if (this.readyState == 4 && this.status != 200) {
+      failure(this);
     }
   };
   xhttp.open(type, uri, true);
